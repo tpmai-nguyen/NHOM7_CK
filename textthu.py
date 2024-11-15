7c78686e2e2070d1cac19c785c98f08c17f7de44
tpmai-nguyen <maintp22413@st.uel.edu.vn>
2024-11-16 00:58:27 +0700

    Update textthu.py

--- textthu.py
+++ textthu.py
@@ -223,6 +223,9 @@ ax.set_title('Correlation Heatmap')
 
 st.pyplot(fig)
 
+delivery_status_data = df_1['delivery_status']
+delivery_status_summary = delivery_status_data.value_counts()
+
 plt.figure(figsize=(10, 6))
 sns.set(style="whitegrid")
 sns.countplot(data=df, x='delivery_status', order=delivery_status_summary.index, palette='coolwarm')
